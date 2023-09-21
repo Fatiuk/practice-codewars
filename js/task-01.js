3da311b602baa7987bb9c36d3477fca29bf704ea
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-09-21 20:21:11 +0300

    Update task 01

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,12 @@
+const exampleString = 'This is an example!';
+
 function reverseWords(str) {
-  // Go for it
+  const reverseString = str
+    .split(' ')
+    .map(word => word.split('').reverse().join(''));
+  return reverseString.join(' ');
 }
+
+reverseWords(exampleString);
+
+console.log(reverseWords(exampleString));
